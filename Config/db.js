2cfdac4f306badc621c2fa0ef54463a885eb1ea8
B Bhanuprasad <bhanuprasadbingi1999@gmail.com>
2025-07-01 10:58:26 +0530

    database connected

--- Config/db.js
+++ Config/db.js
@@ -1,18 +1,20 @@
-
+require('dotenv').config();
 const mysql = require('mysql2');
-const dotenv = require('dotenv');
-dotenv.config();
 
-const db = mysql.createConnection({
+const connection = mysql.createConnection({
   host: process.env.DB_HOST,
+  port: process.env.DB_PORT,
   user: process.env.DB_USER,
-  password: process.env.DB_PASS,
-  database: process.env.DB_NAME,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME
 });
 
-db.connect(err => {
-  if (err) throw err;
-  console.log('MySQL Connected');
+connection.connect((err) => {
+  if (err) {
+    console.error('Database connection failed:', err.stack);
+    return;
+  }
+  console.log('Connected to MySQL database on Clever Cloud!');
 });
 
-module.exports = db;
+module.exports = connection;
