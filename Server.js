02e29b7f7bf4cf3088d8b093c9c33e67f7763eca
B Bhanuprasad <bhanuprasadbingi1999@gmail.com>
2025-07-01 11:13:58 +0530

    port added

--- Server.js
+++ Server.js
@@ -12,5 +12,5 @@ app.use(express.json());
 
 app.use('/api/auth', authRoutes);
 
-const PORT =  5000;
+const PORT =  5000 || process.env.PORT;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
